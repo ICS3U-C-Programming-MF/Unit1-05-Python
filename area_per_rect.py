9618eb1e432fceac19b077d07e09726e2d61eacc
Maximiliano Fairman <Maximiliano.fairman@stu.ocsb.ca>
2025-09-17 17:43:56 +0000

    area_per_rect

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# created by
+# created by
+# this code generated the area and perimeter  of a rectangle
+
+
+def main():
+    print("if a rectangle has the dementions:")
+    print("7cm x 3cm ")
+    print(7 * 3)
+
+
+if __name__ == "__main__":
+    main()
